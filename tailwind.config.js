a0a999f047c1a8551b6e10c05f4b67420892f723
AliOS <ali.ostadhosein@gmail.com>
2023-01-03 02:59:54 +0330

    layout of dashboard completed !

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
       LightGray: '#D3D3D3',
       'desert-storm': '#F8F8F6',
       gray85 : "#D9D9D9",
-      grayGold: "#806600"
+      grayGold: "#806600",
+      blackRussian: "#181B22",
+      stormGray: "#93979D"
     },
 
     fontFamily: {
